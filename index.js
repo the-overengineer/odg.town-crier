7bca34f7b6fa367668e0e7c4b66a8901e007472e
Luka Skukan <luka.skukan@happening.xyz>
2025-04-14 12:57:05 +0200

    refactor: Use ODG api for fetching notifs

--- index.js
+++ index.js
@@ -4,25 +4,16 @@ require('dotenv').config({
     path: join(__dirname, '.env.local'),
 });
 
-const { crawlLatestGames } = require('./lib/game-crawler');
-const { sendGamesTomorrowMessage, sendEmptyGamesInTwoDaysMessage } = require('./lib/discord-bot');
-const { isTomorrow, isInDays } = require('./lib/utils');
+const { getGameLists } = require('./lib/game-crawler');
+const { sendGamesTomorrowMessage } = require('./lib/discord-bot');
 
 (async () => {
-    const games = await crawlLatestGames();
-    // Only select games that are tomorrow
-    const gamesHappeningTomorrow = games.filter((game) => isTomorrow(game.date));
-    const gamesWithEmptySpotsInTwoDays = games.filter((game) => isInDays(game.date, 2) && game.players.length < game.spots);
+    const games = await getGameLists();
+    const gamesToday = games.games_today;
 
-    if (gamesHappeningTomorrow.length > 0) {
+    if (gamesToday.length > 0) {
         await sendGamesTomorrowMessage(gamesHappeningTomorrow);
     } else {
-        console.log('No games tomorrow');
-    }
-
-    if (gamesWithEmptySpotsInTwoDays.length > 0) {
-        await sendEmptyGamesInTwoDaysMessage(gamesWithEmptySpotsInTwoDays);
-    } else {
-        console.log('No empty games in two days');
+        console.log('No games today');
     }
 })();
--- upcoming-games.js
+++ upcoming-games.js
@@ -4,31 +4,15 @@ require('dotenv').config({
     path: join(__dirname, '.env.local'),
 });
 
-const { crawlLatestGames } = require('./lib/game-crawler');
+const { getGameLists } = require('./lib/game-crawler');
 const { sendApplicationsOpenTonightMessage } = require('./lib/discord-bot');
-const { isInDays } = require('./lib/utils');
-const { isMonday, isSaturday } = require('date-fns');
 
 (async () => {
-    const games = await crawlLatestGames();
-    const gamesNextWeek = games.filter((game) => {
-        const today = new Date();
+    const games = await getGameLists();
+    const gamesOpening = games.games_open;
 
-        // Temporary hack for new season
-        if (isMonday(today) && game.odg === 'ODG122') {
-            return isInDays(game.date, 5);
-        }
-
-        if (game.odg === 'ODG122') {
-            return false;
-        }
-
-        return isSaturday(today) &&
-            (isInDays(game.date, 7) || isInDays(game.date, 9));
-    });
-
-    if (gamesNextWeek.length > 0) {
-        const odgName = gamesNextWeek[0].odg;
+    if (gamesOpening.length > 0) {
+        const odgName = gamesOpening[0].odg;
         const applicableGames = games.filter((game) => game.odg === odgName);
 
         await sendApplicationsOpenTonightMessage(applicableGames);
