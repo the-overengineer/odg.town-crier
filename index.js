43e5b720b3ab86494338b9d65bd8156e8ed6cc57
Luka Skukan <luka.skukan@happening.xyz>
2025-04-14 13:33:04 +0200

    Add games open bot

--- index.js
+++ index.js
@@ -5,15 +5,29 @@ require('dotenv').config({
 });
 
 const { getGameLists } = require('./lib/game-crawler');
-const { sendGamesTodayMessage } = require('./lib/discord-bot');
+const { differenceInDays } = require('date-fns');
+const { sendGamesTodayMessage, sendEmptyGamesMessage } = require('./lib/discord-bot');
 
 (async () => {
-    const games = await getGameLists();
+    const today = process.argv[2] ? new Date(process.argv[2]) : new Date();
+    const games = await getGameLists(process.argv[2]);
     const gamesToday = games.games_today;
+    const gamesWithOpenSpots = games.games_with_open_spots;
+
+    const openSpotsInTwoDays = gamesWithOpenSpots.filter((game) => {
+        const daysDifference = differenceInDays(game.date, today);
+        return daysDifference === 2;
+    });
 
     if (gamesToday.length > 0) {
         await sendGamesTodayMessage(gamesHappeningTomorrow);
     } else {
         console.log('No games today');
+    }
+
+    if (openSpotsInTwoDays.length > 0) {
+        await sendEmptyGamesMessage(openSpotsInTwoDays);
+    } else {
+        console.log('No games with open spots in two days');
     }
 })();
